ba09ad310ce61ada4bbdc9f30208e2811581e819
lourod1987 <louart87@gmail.com>
2018-07-07 22:55:10 -0500

    setting up CreateJS library for game

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,19 @@
 class Game {
     constructor() {
         console.log(`Welcome to the game. Version ${this.version()}`);
+
+        this.canvas = document.getElementById("game-canvas");
+        this.stage = new createjs.Stage(this.canvas);
+
+        createjs.Ticker.setFPS(60);
+        //redraw stage at 60fps
+        createjs.Ticker.on("tick", this.stage);
+
+        //for testing
+        const circle = new createjs.Shape();
+        circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+        circle.x = circle.y = 100;
+        this.stage.addChild(circle);
     }
 
     version() {
